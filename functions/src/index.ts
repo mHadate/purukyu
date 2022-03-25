467fe20663f00842717878105e3c50c1c0c1d901
mHadate <masamichi.hadate@gmail.com>
2022-03-25 13:11:23 +0900

    add firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,15 @@
+import functions from "./libs/functions"
+import admin from "./libs/firebase"
+import stripe from "./libs/stripe"
+
+export const createStripeCustomer = functions.auth.user().onCreate(async (user) => {
+  const customer = await stripe.customers.create({ name: user.displayName });
+  return admin
+    .firestore()
+    .collection("stripe_customers")
+    .doc(user.uid)
+    .set({
+      customer_id: customer.id,
+      name: user.displayName
+    });
+});
