325e966264003478e64af92cce0565ee05ba08c3
mHadate <masamichi.hadate@gmail.com>
2022-04-06 15:59:58 +0900

    add email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import admin from "./libs/firebase"
 import stripe from "./libs/stripe"
 
 export const createStripeCustomer = functions.auth.user().onCreate(async (user) => {
-  const customer = await stripe.customers.create({ name: user.displayName });
+  const customer = await stripe.customers.create({ name: user.displayName, email: user.email });
   return admin
     .firestore()
     .collection("stripe_customers")
