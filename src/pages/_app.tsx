6125caa36cd23067d4d6ae2294d6a1346a2daf84
mHadate <masamichi.hadate@gmail.com>
2022-04-06 15:59:42 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app";
 import { Layout } from "../layout";
 import "../../styles/globals.css";
 import "../../styles/styles.css";
-import { useAuthentication } from "../hooks/authenication";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
