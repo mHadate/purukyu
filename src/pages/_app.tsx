723e4a599234301442f1164e40b3d9242578e88f
mHadate <masamichi.hadate@gmail.com>
2022-05-13 18:36:28 +0900

    create admin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,22 @@ import type { AppProps } from "next/app";
 import { Layout } from "../layout";
 import "../../styles/globals.css";
 import "../../styles/styles.css";
-import { AuthProvider } from "../context/AuthContext";
+import { AuthProvider, AdminAuthProvider } from "../context/AuthContext";
+import { useRouter } from "next/router";
+import { AdminLayout } from "./admin/layout";
+import { init } from "../services/adminApi";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
+  init()
+  const router = useRouter();
+  const pathname = router.pathname;
+  return pathname.match(/^\/admin/) ? (
+    <AdminAuthProvider>
+      <AdminLayout>
+        <Component {...pageProps} />
+      </AdminLayout>
+    </AdminAuthProvider>
+  ) : (
     <AuthProvider>
       <Layout>
         <Component {...pageProps} />
