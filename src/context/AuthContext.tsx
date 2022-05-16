9dd03aee11697ab62d25083a45ce0aa808b84642
mHadate <masamichi.hadate@gmail.com>
2022-05-16 15:28:38 +0900

    fix

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,6 @@
 import { createContext, useEffect, useState, useContext } from "react";
 import { User } from "../types/user";
 import { onAuthStateChanged } from "../services/firebase";
-import { apiAgent } from "../services/adminApi";
-import Router from "next/router";
 
 type AuthContextProps = {
   user: User | null | undefined;
@@ -25,25 +23,3 @@ export const AuthProvider: React.FC = ({ children }) => {
 };
 
 export const useAuthContext = () => useContext(AuthContext);
-
-export const AdminAuthProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<User | null | undefined>(undefined);
-
-  useEffect(() => {
-    const token = localStorage.getItem("credentical");
-    (async (token) => {
-      apiAgent.setHeaders({ Authorization: `Bearer ${token}` });
-      const result = await apiAgent.get("/login/status", {});
-      if (result?.status !== "ok") {
-        localStorage.removeItem("credentical");
-        Router.push("/admin")
-      }
-    })(token);
-  }, []);
-
-  return (
-    <AuthContext.Provider value={{ user: user }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
