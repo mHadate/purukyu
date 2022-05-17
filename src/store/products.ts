04131887afe9bd068db55220610c49415abc53c7
mHadate <masamichi.hadate@gmail.com>
2022-05-17 17:46:41 +0900

    fix stripe store

--- src/store/products.ts
+++ src/store/products.ts
@@ -1,17 +1,14 @@
 import Stripe from "stripe";
-
-const webapp_url = process.env.WEBAPP_URL
+import stripe from "../services/stripe"
 
 export const getProducts = async (): Promise<ProductList> => {
-  const res = await fetch(`${webapp_url}/api/products`);
-  return await res.json();
+  return await stripe.products.list({ limit: 50 })
 };
 
 export const getProduct = async (
   productId: string
 ): Promise<Stripe.Product> => {
-  const res = await fetch(`${webapp_url}/api/products/${productId}`);
-  return await res.json();
+  return await stripe.products.retrieve(productId)
 };
 
 export type ProductList = Stripe.ApiList<Stripe.Product> | null;
