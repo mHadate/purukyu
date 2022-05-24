0ee5cd351e20cf270895005edbdb4015db33e7d4
mHadate <masamichi.hadate@gmail.com>
2022-05-24 18:30:52 +0900

    notification service

--- src/services/slack.ts
+++
@@ -1,8 +0,0 @@
-export const sendSlackWebhook = async (message: string) => {
-  const url = process.env.SLACK_WEBHOOK_URL;
-  if (!url) return;
-  await fetch(url, {
-    method: "POST",
-    body: JSON.stringify({text: message})
-  });
-}
