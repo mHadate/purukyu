edf00d120978d386e21ac1bb8d915d98b28d8592
mHadate <masamichi.hadate@gmail.com>
2022-05-24 16:56:34 +0900

    fix date

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -44,7 +44,7 @@ const createNowFromStartDateList = () => {
 const dateFormat = (date: Date) => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDay().toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
   const hour = date.getHours();
   const min = date.getMinutes();
   return `${year}-${month}-${day} ${hour}:${min}`;
