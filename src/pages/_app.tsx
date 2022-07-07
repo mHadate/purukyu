56d8e8117c67fa24baad7e76c417e797d56f415b
mHadate <masamichi.hadate@gmail.com>
2022-07-07 23:24:35 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,22 +4,11 @@ import "bulma/css/bulma.min.css";
 import "../../styles/globals.css";
 import "../../styles/styles.css";
 import { AuthProvider } from "../context/AuthContext";
-import { AdminAuthProvider } from "../context/AdminAuthContext";
-import { useRouter } from "next/router";
-import AdminLayout from "./admin/layout";
 import { init } from "../services/adminApi";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   init();
-  const router = useRouter();
-  const pathname = router.pathname;
-  return pathname.match(/^\/admin/) ? (
-    <AdminAuthProvider>
-      <AdminLayout>
-        <Component {...pageProps} />
-      </AdminLayout>
-    </AdminAuthProvider>
-  ) : (
+  return (
     <AuthProvider>
       <Layout>
         <Component {...pageProps} />
