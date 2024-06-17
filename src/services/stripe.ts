d981e5a4d435ab4542be8b24753fb28eb6c2f68f
mHadate <masamichi.hadate@gmail.com>
2024-06-17 19:19:40 +0900

    update stripe api version

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 const secretKey = process.env.STRIPE_API_SECRET_KEY as string;
 
 const stripe = new Stripe(secretKey, {
-  apiVersion: "2020-08-27",
+  apiVersion: "2024-04-10",
 });
 
 export default stripe;
